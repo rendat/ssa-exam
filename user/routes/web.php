9b07a4a02aefc7ae91a80c137dace4d1d3f5bdfe
Rainer Miguel <95733449+rendat@users.noreply.github.com>
2024-08-31 09:05:18 +0800

    Middleware for routes

--- user/routes/web.php
+++ user/routes/web.php
@@ -27,22 +27,15 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 
 
+
+Route::middleware(['auth'])->group(function () {
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
 Route::get('/users/{user}', [UserController::class, 'show'])->name('users.show');
 Route::get('/users/create', [UserController::class, 'create'])->name('users.create');
 Route::post('/users', [UserController::class, 'store'])->name('users.store');
-// Web Routes
 Route::get('/users/{user}/edit', [UserController::class, 'edit'])->name('users.edit');
 Route::put('/users/{user}', [UserController::class, 'update'])->name('users.update');
-
-
-
-
-
-
 Route::delete('/users/{user}', [UserController::class, 'destroy'])->name('users.destroy');
-
 Route::get('/trashed', [UserController::class, 'trasheds'])->name('users.trashed');
-
-
 Route::softDeletes('users', UserController::class);
+});
